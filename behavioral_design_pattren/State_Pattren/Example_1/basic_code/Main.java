036ca309b2e4f2b99e0565f9a391a27123405807
chaithanya <maramreddychaitu2005@gmail.com>
2026-03-23 00:44:29 +0530

    state pattren basic code

---
+++ behavioral_design_pattren/State_Pattren/Example_1/basic_code/Main.java
@@ -0,0 +1,10 @@
+public class Main{
+ public static void main(String[] args) {
+  MusicPlayer player=new MusicPlayer();
+  player.play();
+  player.play();
+  player.pause();
+  player.play();
+  
+ } 
+}
